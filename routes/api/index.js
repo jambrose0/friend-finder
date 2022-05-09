f93746c5435272d120bda317028685abb607ae00
jambrose0 <jambrose0518@gmail.com>
2022-05-08 23:09:32 -0400

    created all controls and routes for thoughts and reactions

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,9 @@
 const router = require("express").Router();
-// const apiRoutes = require("./api");
 const userRoutes = require("./user-routes");
 const thoughtRoutes = require("./thought-routes");
 
 //user routes
 router.use("/user", userRoutes);
-
-//thought routes
 router.use("/thoughts", thoughtRoutes);
 
 module.exports = router;
